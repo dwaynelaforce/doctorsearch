2c0b68220b7bf48bfa70ba265b84179c5804608c
Dwayne LaForce <72627037+dwaynelaforce@users.noreply.github.com>
2024-03-17 23:33:22 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,7 @@ from wa import license_search
 
 class TestDoctorSearch(unittest.TestCase):
 
-    def test_wa(self):
-        searchdoctor = Doctor("Lai", "Kin", degree=MedicalDegree.MD)
-        results = license_search(searchdoctor)
-        self.assertEqual(results.status, search.QueryStatus.SUCCESS)
+    ...
 
 if __name__ == "__main__":
     unittest.main()
